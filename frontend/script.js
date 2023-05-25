a0e6cd0ee0ccd58332f68b430d64ff84dd3faf85
Joaquin Brusco <Joaquinbrusco1@gmail.com>
2023-05-25 02:32:49 -0300

    Cambios en el estilo de la pagina, Conexion con Axios, Nueva funcionalidad para actualizar datos

---
+++ frontend/script.js
@@ -0,0 +1,93 @@
+function newBus() {
+  const matricula = document.querySelector("#matricula").value;
+  const marca = document.querySelector("#marca").value;
+  const pasajeros = document.querySelector("#pasajeros").value;
+  const altura = document.querySelector("#altura").value;
+  const peso = document.querySelector("#peso").value;
+  const largo = document.querySelector("#largo").value;
+  const ejes = document.querySelector("#ejes").value;
+  const empresa = document.querySelector("#empresa").value;
+
+  const data = {
+    matricula,
+    marca,
+    pasajeros,
+    altura,
+    peso,
+    largo,
+    ejes,
+    empresa
+  };
+
+  // axios
+  //   .post("", data)
+  //   .then(function (response) {
+  //     console.log(response.data);
+  //   })
+  //   .catch(function (error) {
+  //     console.error(error);
+  //   });
+}
+
+  function createBus(){
+
+    axios
+    .get('https://jsonplaceholder.typicode.com/posts')
+    .then(response => {
+      response.data.forEach(element => {
+        let userData = {
+          userIdData: element.userId, 
+          IdData: element.id,
+          titleData: element.title, 
+          bodyData: element.body
+        }
+        const busProperties = [
+          { name: "matricula", value:userData.IdData },
+          { name: "marca", value:userData.userIdData },
+          { name: "pasajeros", value:userData.userIdData },
+          { name: "altura", value:userData.userIdData},
+          { name: "peso", value:userData.userIdData},
+          { name: "largo", value:userData.userIdData},
+          { name: "ejes", value:userData.userIdData},
+          { name: "empresa", value:userData.userIdData}
+        ];
+  
+        
+        const busDiv = document.createElement("div");
+        busDiv.classList.add("bus");
+        document.querySelector(".buses-div").appendChild(busDiv);
+        for (i = 0; i < busProperties.length; i++) {
+          const busP = document.createElement("p");
+          const busSpan1 = document.createElement("span");
+          const br = document.createElement("br");
+          const busSpan2 = document.createElement("span");
+          
+          busDiv.appendChild(busP);
+          busP.appendChild(busSpan1);
+          busP.appendChild(br);
+          busP.appendChild(busSpan2);
+          
+          busSpan1.classList.add("item-name");
+          busSpan2.classList.add("item-value");
+          busSpan1.innerHTML = busProperties[i].name;
+          busSpan2.innerHTML = busProperties[i].value;
+        }
+        
+      });
+  
+
+    }).catch(error => {
+      console.error('Error sending data:', error);
+    });
+
+  }
+
+  createBus();
+  function refresh() {
+    const busDivs = document.querySelectorAll(".buses-div .bus");
+  
+    busDivs.forEach(busDiv => {
+      busDiv.remove();
+    });
+    createBus();
+  }
